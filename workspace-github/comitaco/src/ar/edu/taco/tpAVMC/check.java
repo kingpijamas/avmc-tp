ad4573a6784b50a7eaa976d270da9cda1b61b9f4
Teresa Fontanella De Santis <teresa@Teresas-MacBook-Pro.local>
2015-12-03 20:17:55 -0300

    adding check annotation in tpAVMC package

---
+++ workspace-github/comitaco/src/ar/edu/taco/tpAVMC/check.java
@@ -0,0 +1,13 @@
+package ar.edu.taco.tpAVMC;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface check {
+    
+   String value();
+}
---
+++ workspace-github/comitaco/src/ar/edu/taco/tpAVMC/lalaAnnotated.java
@@ -0,0 +1,9 @@
+package ar.edu.taco.tpAVMC;
+
+class lalaAnnotated {
+    
+    @check("Arithmetic")
+    public void foo(String myParam) {
+       System.out.println("This is " + myParam);
+    }
+ }
